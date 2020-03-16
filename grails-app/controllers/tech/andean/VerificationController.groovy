40dd376c6438122f3c6e29beead0465fbbdcc925
Alex Oviedo <alexove@andean.tech>
2020-03-16 07:22:22 -0500

    Some fiex

--- grails-app/controllers/tech/andean/VerificationController.groovy
+++ grails-app/controllers/tech/andean/VerificationController.groovy
@@ -5,32 +5,23 @@ class VerificationController {
     static allowedMethods=[index:['GET','POST']]
 
     def index(String lastName,String serialNumber) {
+        if(lastName!=null && serialNumber!=null){
 
-        def c = Certificate.withCriteria{
-            eq("serialNumber",serialNumber,[ignoreCase: true])
-            and {
-                enrollment {
-                    student {
-                            eq("lastName",lastName,[ignoreCase: true])
+            def c = Certificate.withCriteria{
+                eq("serialNumber",serialNumber,[ignoreCase: true])
+                and {
+                    enrollment {
+                        student {
+                                eq("lastName",lastName,[ignoreCase: true])
+                        }
                     }
                 }
             }
-        }
-
-        render "===> ${c}"
-
-        def certificate = Certificate.findBySerialNumber(serialNumber)
-        if(certificate!=null){
-            render "====> Certificado encontrado"
-            // if((student=certificate.enrollment.student)!=null){
-            //     if(student.lastName.equals(lastName)){
-            //         // TODO: Sent to information view
-            //         println "Certificate information CORRECT"
-            //     }
-            // }// ELSE show a error message
-        }else{
-            // TODO: Show certificate information error
-            render "=====> Certificate information ERROR"
+            if(c.size()==1){
+                render "SI EXISTE => ${c.get(0).enrollment.student.name}"
+            }else{
+                render "NO EXISTE"
+            }
         }
     }
 }
---
+++ grails-app/views/verification/index.gsp
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="es">
+    <head>
+        <meta name="layout" content="main">
+        <title>Verificación de Certificados</title>
+    </head>
+    <body>
+        <div class="container">
+            <div class="row">
+                <div class="col-md-8">
+                    .
+                </div>
+                <div class="col-md-4">
+                    <g:form name="name" action="action">
+                        <div class="form-group">
+                            <label for="">Apellidos *</label>
+                            <input type="text" name="" value="" class="form-control">
+                        </div>
+                        <div class="form-group">
+                            <label for="">Serie del Certificado *</label>
+                            <input type="text" name="" value="" class="form-control">
+                        </div>
+                        <div class="form-group form-check">
+                            <input type="checkbox" name="" value="" class="form-check-input">
+                            <label for="" class="form-check-label"  >Acepto los terminos y condiciones del <a href="#">contrato </a></label>
+                        </div>
+                    </g:form>
+                </div>
+
+            </div>
+        </div>
+    </body>
+</html>
